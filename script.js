bd3d3295017eb009fcd7469fc3b4ce0373894cc7
joshoffone <12georgej@gmail.com>
2025-11-17 15:50:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   const el = document.getElementById('quote');
   if (!el) return;
   try {
-    const res = await fetch('https://api.quotable.io/random');
+    const res = await fetch('https://dailyverses.net/random-bible-verse');
     const data = await res.json();
     el.textContent = `"${data.content}" — ${data.author}`;
   } catch {
